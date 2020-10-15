cbad9c806011adb1df078f075fbf09439d3c61ef
xjlonly <xjlonly@foxmail.com>
2020-10-15 22:23:13 +0800

    add mvc

--- src/main/java/org/example/mv/web/UserController.java
+++ src/main/java/org/example/mv/web/UserController.java
@@ -1,4 +1,19 @@
 package org.example.mv.web;
 
+import org.example.mv.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class UserController {
+    @Autowired
+    UserService userService;
+
+
+    @GetMapping("/")
+    public ModelAndView index(){
+        return new ModelAndView();
+    }
 }
